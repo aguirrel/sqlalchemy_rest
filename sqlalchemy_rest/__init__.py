fc86f57bfc75f763ac5af73e3e80ec653fbdbc88
Luis Aguirre <luis@aguirrel.com.ar>
2014-08-20 11:25:23 -0300

    Changing name sqla2json to to_dict

--- sqlalchemy_rest/__init__.py
+++ sqlalchemy_rest/__init__.py
@@ -5,6 +5,6 @@ Base = None
 from .json import to_dict, from_dict
 
 def config_sqlalchemy_rest(base, db_session):
-    base.to_dict = sqla2json
+    base.to_dict = to_dict
     base.from_dict = from_dict
     DBSession = db_session
