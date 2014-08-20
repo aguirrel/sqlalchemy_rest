469f07b92ba73debb9191dea1d9035ff5f7c0d6c
Luis Aguirre <luis@aguirrel.com.ar>
2014-08-20 11:36:29 -0300

    DBSession set is global

--- sqlalchemy_rest/__init__.py
+++ sqlalchemy_rest/__init__.py
@@ -7,4 +7,4 @@ from .json import to_dict, from_dict
 def config_sqlalchemy_rest(base, db_session):
     base.to_dict = to_dict
     base.from_dict = from_dict
-    DBSession = db_session
+    global DBSession = db_session
