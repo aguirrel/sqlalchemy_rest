f76aa1c90bc0f2f085d2c2977a753074db1a84df
Luis Aguirre <luis@aguirrel.com.ar>
2014-09-29 09:11:48 -0300

    Default count is max count

--- sqlalchemy_rest/rest/__init__.py
+++ sqlalchemy_rest/rest/__init__.py
@@ -32,7 +32,7 @@ class AlchemyBaseRest(object):
         self.request = request
 
     def collection_get(self):
-        count = 10
+        count = max_count
         page = 1
 
         db_query = DBSession.query(self.DBClass)
